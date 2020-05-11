b3847f2e1e7f10869f16349458ce9d8201996f87
subeans <39542978+subeans@users.noreply.github.com>
2020-05-11 18:19:38 +0900

    Update Create_new_MLP.py

--- notebooks/Create_new_MLP.py
+++ notebooks/Create_new_MLP.py
@@ -4,8 +4,10 @@ import mlpredict
 MLP = mlpredict.api.dnn(input_dimension=1, input_size=28)
 
 
-MLP.add_layer('Fully_connected','hidden_layer',activation='relu',neurons='128')
-MLP.add_layer('Fully_connected', 'output_layer', activation='softmax',neurons='10')
+MLP.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=128, 
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+MLP.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=10 
+                padding='SAME', strides=1, use_bias=1, activation='softmax')
 
 
 MLP.describe()
