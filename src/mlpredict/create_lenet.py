453ffb92c5746d4723e4cf69a4dd0db977ba9981
subeans <39542978+subeans@users.noreply.github.com>
2020-05-14 18:20:35 +0900

    Update create_lenet.py

--- src/mlpredict/create_lenet.py
+++ src/mlpredict/create_lenet.py
@@ -8,19 +8,17 @@ Lenet5.add_layer('Convolution', 'conv1_1', kernelsize=3, channels_out=6,
                 padding='SAME', strides=1, use_bias=1, activation='relu')
                 
                 
-Lenet5.add_layer('Average_pool', 'pool1', pool_size=2, padding='SAME', strides=2)
+Lenet5.add_layer('Max_pool', 'pool1', pool_size=2, padding='SAME', strides=2)
 
 
 Lenet5.add_layer('Convolution', 'conv2_1', kernelsize=3, channels_out=16, 
                 padding='SAME', strides=1, use_bias=1, activation='relu')
-#flatten
-Lenet5 = mlpredict.api.dnn(input_dimension=1, input_size=28)
-                
+
 Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=120, 
                 padding='SAME', strides=1, use_bias=1, activation='relu')
-Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=84, 
+Lenet5.add_layer('Convolution', 'fc2', kernelsize=1, channels_out=84, 
                 padding='SAME', strides=1, use_bias=1, activation='relu')
-Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=10, 
+Lenet5.add_layer('Convolution', 'fc3', kernelsize=1, channels_out=10, 
                 padding='SAME', strides=1, use_bias=1, activation='softmax')
 
 Lenet5.describe()
