c8a3b6df089da493d114f208bed3cc58033b83b2
subeans <39542978+subeans@users.noreply.github.com>
2020-05-14 16:44:31 +0900

    Create prediction_lenet5.py

---
+++ src/mlpredict/prediction_lenet5.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+import mlpredict
+
+
+gpu = 'V100'
+opt = 'SGD'
+
+Lenet5 = mlpredict.import_tools.import_dnn('Lenet5')
+
+Lenet5.describe()
+
+batchsize = 2**np.arange(0,10,1)
+time_layer = np.zeros([5,10])
+time_total = np.zeros(10)
+
+for i in range(len(batchsize)):
+    time_total[i], layer, time_layer[:,i] = Lenet5.predict(gpu = gpu,
+                                                          optimizer = opt,
+                                                          batchsize = batchsize[i])
+    print ("time_total = " ,time_total[i])
