992f0bcb1472b7df3be14b249b2ac938004bc5f4
subeans <39542978+subeans@users.noreply.github.com>
2020-05-14 16:30:49 +0900

    Create create_mlp.py

---
+++ src/mlpredict/create_mlp.py
@@ -0,0 +1,9 @@
+import mlpredict
+MLP = mlpredict.api.dnn(input_dimension=1, input_size=28)
+
+MLP.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=128,
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+MLP.add_layer('Convolution', 'fc2', kernelsize=1, channels_out=10,
+                padding='SAME', strides=1, use_bias=1, activation='softmax')
+MLP.describe()
+MLP.save('mlpredict/dnn_architecture/MLP.json') 
