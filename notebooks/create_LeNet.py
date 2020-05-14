6c330269505e497f8eb2c8cdfc1499025571b47a
subeans <39542978+subeans@users.noreply.github.com>
2020-05-14 16:30:18 +0900

    Create create_LeNet.py

---
+++ notebooks/create_LeNet.py
@@ -0,0 +1,27 @@
+import mlpredict
+
+
+Lenet5 = mlpredict.api.dnn(input_dimension=1, input_size=28)
+
+
+Lenet5.add_layer('Convolution', 'conv1_1', kernelsize=3, channels_out=6, 
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+                
+                
+Lenet5.add_layer('Average_pool', 'pool1', pool_size=2, padding='SAME', strides=2)
+
+
+Lenet5.add_layer('Convolution', 'conv2_1', kernelsize=3, channels_out=16, 
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+#flatten
+Lenet5 = mlpredict.api.dnn(input_dimension=1, input_size=28)
+                
+Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=120, 
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=84, 
+                padding='SAME', strides=1, use_bias=1, activation='relu')
+Lenet5.add_layer('Convolution', 'fc1', kernelsize=1, channels_out=10, 
+                padding='SAME', strides=1, use_bias=1, activation='softmax')
+
+Lenet5.describe()
+Lenet5.save('mlpredict/dnn_architecture/Lenet5.json')
